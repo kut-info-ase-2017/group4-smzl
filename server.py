08bfb302be2f0c81a5a7ccd37c1de7cb129ab1ae
hiroyasu.fujita <hiroyasu.fujita@nextremer.com>
2017-09-24 20:40:44 +0900

    通信と顔認識処理の連携

--- server.py
+++ server.py
@@ -1,21 +1,79 @@
 # -*- coding:utf-8 -*-
 import socket
+import sys
+sys.path.append('./face_recognition')
+import camera
+import face_recognize
 
-host = "172.21.32.101" #server ip
-port = 8080 #port  same client
+def main(method='eigen'):
+    recognizer = None
+    if method == 'eigen':
+        #### 固有顔法 ####
 
-serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serversock.bind((host,port)) #bind ip and port
-serversock.listen(10) #connect listen（Max queue）
+        # データ準備
+        ## トレーニング時のラベル割り当てと揃える
+        user_dict = {'goda':0, 'tada':1, 'yasumitsu':2, 'unkown':3}
+        eigen_model = '/Users/hirto/Desktop/Advanced_SE/implementation/eigenface/recognizer.face'
 
-print('Waiting for connections...')
-clientsock, client_address = serversock.accept()
+        # モデル準備
+        recognizer = Eigen_FaceRecognizer(eigen_model, user_dict)
 
-while True:
-    rcvmsg = clientsock.recv(1024)
-    if rcvmsg != b'':
-    	print('Received -> %s' % (rcvmsg))
-    #print('Wait...')
+    elif method == 'alexnet':
+        #### CNN (AlexNet) ####
 
-clientsock.close()
+        # データ準備
+        cnn_model = '/Users/hirto/Desktop/Advanced_SE/implementation/example/face_prediction-master/cnn_4.model'
+        net = AlexNet
+
+        # モデル準備
+        recognizer = CNN_FaceRecognizer(net, cnn_model)
+
+    elif method == 'vggfacenet':
+        #### CNN (VggFaceNet) ####
+        # データ準備
+        cnn_model = '/Users/hirto/Desktop/Advanced_SE/implementation/example/oxford/finetuning_4.model'
+        net = VggFaceNet
+
+        # モデル準備
+        recognizer = CNN_FaceRecognizer(net, cnn_model)
+
+    # while True:
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+
+    face = cam.get_face()
+    user = 'no user'
+    if face is not None:
+        user = recognizer.predict(face)
+    return user
+
+
+
+if __name__ == '__main__':
+    cascade = '/Users/hirto/anaconda/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
+    cam = camera.Camera(cascade=cascade)
+
+    # 座席表示用の名前辞書
+    index = {'goda': '合田', 'tada':'多田', 'yasumitsu':'安光', 'unkown':'I am tada (^^)/'}
+
+
+    host = "172.21.32.101" #server ip
+    port = 8080 #port  same client
+
+    serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversock.bind((host,port)) #bind ip and port
+    serversock.listen(10) #connect listen（Max queue）
+
+    print('Waiting for connections...')
+    clientsock, client_address = serversock.accept()
+
+    while True:
+        rcvmsg = clientsock.recv(1024)
+        if rcvmsg == b'detect':
+            # gui側の名前セット用関数を呼び出したい
+        	print(index[main()])
+        #print('Wait...')
+
+    clientsock.close()
+    cam.close()
