e10c54914658748866d5852c216c5a8687359849
hiroyasu.fujita <hiroyasu.fujita@nextremer.com>
2017-09-25 10:26:42 +0900

    モデルパス変更

--- server.py
+++ server.py
@@ -2,10 +2,16 @@
 import socket
 import sys
 sys.path.append('./face_recognition')
+sys.path.append('./face_recognition/cnn')
 import camera
 import face_recognize
+from net_model import AlexNet
+from net_model import VggFaceNet
+from image2TrainAndTest import image2TrainAndTest
+from image2TrainAndTest import getValueDataFromPath
+from image2TrainAndTest import getValueDataFromImg
 
-def main(method='eigen'):
+def main(method='alexnet'):
     recognizer = None
     if method == 'eigen':
         #### 固有顔法 ####
@@ -16,7 +22,7 @@ def main(method='eigen'):
         eigen_model = '/Users/hirto/Desktop/Advanced_SE/implementation/eigenface/recognizer.face'
 
         # モデル準備
-        recognizer = Eigen_FaceRecognizer(eigen_model, user_dict)
+        recognizer = face_recognize.Eigen_FaceRecognizer(eigen_model, user_dict, threshold=90)
 
     elif method == 'alexnet':
         #### CNN (AlexNet) ####
@@ -26,16 +32,16 @@ def main(method='eigen'):
         net = AlexNet
 
         # モデル準備
-        recognizer = CNN_FaceRecognizer(net, cnn_model)
+        recognizer = face_recognize.CNN_FaceRecognizer(net, cnn_model)
 
     elif method == 'vggfacenet':
         #### CNN (VggFaceNet) ####
         # データ準備
-        cnn_model = '/Users/hirto/Desktop/Advanced_SE/implementation/example/oxford/finetuning_4.model'
+        cnn_model = '/Users/hirto/Desktop/Advanced_SE/implementation/example/oxford/finetuning33_4.model'
         net = VggFaceNet
 
         # モデル準備
-        recognizer = CNN_FaceRecognizer(net, cnn_model)
+        recognizer = face_recognize.CNN_FaceRecognizer(net, cnn_model)
 
     # while True:
     #     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -50,14 +56,14 @@ def main(method='eigen'):
 
 
 if __name__ == '__main__':
-    cascade = '/Users/hirto/anaconda/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml'
-    cam = camera.Camera(cascade=cascade)
+
+    cam = camera.Camera()
 
     # 座席表示用の名前辞書
-    index = {'goda': '合田', 'tada':'多田', 'yasumitsu':'安光', 'unkown':'I am tada (^^)/'}
+    index = {'goda': '合田', 'tada':'多田', 'yasumitsu':'安光', 'unknown':'I am tada (^^)/'}
 
 
-    host = "172.21.32.101" #server ip
+    host = "172.21.32.54" #server ip
     port = 8080 #port  same client
 
     serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -71,9 +77,12 @@ if __name__ == '__main__':
     while True:
         rcvmsg = clientsock.recv(1024)
         if rcvmsg == b'detect':
-            # gui側の名前セット用関数を呼び出したい
-        	print(index[main()])
-        #print('Wait...')
+            try:
+                # gui側の名前セット用関数を呼び出したい
+            	print(index[main()])
+                #print('Wait...')
+            except Exception as e:
+                print(e)
 
     clientsock.close()
     cam.close()
